9e394fd85621f8f8a3371bbbf77874b76efe9200
SangWon7242 <gotkdqja@gmail.com>
2024-11-19 17:47:44 +0900

    junit, assertj 도입

--- src/test/java/AppTest.java
+++ src/test/java/AppTest.java
@@ -1,5 +1,11 @@
-public class AppTest {
-  public static void main(String[] args) {
+import org.junit.jupiter.api.Test;
 
+import static org.assertj.core.api.Assertions.assertThat;
+
+public class AppTest {
+  @Test
+  void 테스트_assertThat() {
+    int rs = 10 + 20;
+    assertThat(rs).isEqualTo(30);
   }
 }
